0110d7a257635d83ff082bc31cd016cfe7a33a59
JulianArbini97 <1890@holbertonschool.com>
2020-12-07 16:01:27 +0000

    python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks for a cycle in singly linked list
+ * @list: input head
+ * Retrun: 0 or 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temporal;
+
+	if (list = 0)
+	{
+		return (0);
+	}
+
+	while (list != 0)
+	{
+		temporal = list;
+		list = list->next;
+		if (temporal <= list)
+		{
+			return (1);
+		}
+	}
+	return(0);
+}
