0b382745e423e19720c92c59e2205c3fde25e507
JulianArbini97 <1890@holbertonschool.com>
2020-12-07 16:06:09 +0000

    python

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,12 +12,12 @@ int check_cycle(listint_t *list)
 {
 	listint_t *temporal;
 
-	if (list != 0)
+	if (!list)
 	{
 		return (0);
 	}
 
-	while (list = 0)
+	while (list)
 	{
 		temporal = list;
 		list = list->next;
