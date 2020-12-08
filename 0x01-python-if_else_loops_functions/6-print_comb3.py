4858b156816de37540e2fa5d2a36537f75aa8b79
JulianArbini97 <1890@holbertonschool.com>
2020-12-08 15:41:35 +0000

    python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+for first in range (0, 9):
+       second = first + 1
+       for second in range (second, 9):
+              print("{:d}".format(first), end ="")
+              print("{:d}, ".format(second), end ="")
+print("89")
