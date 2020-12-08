6cf278ccbdedada80bcb64d59b5da348c90092d2
JulianArbini97 <1890@holbertonschool.com>
2020-12-08 17:24:31 +0000

    python

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for s in range (1, 101):
+    for s in range(1, 101):
         if (s % 15 == 0):
             print("FizzBuzz", end=" ")
         elif (s % 3 == 0):
@@ -9,4 +9,3 @@ def fizzbuzz():
             print("Buzz", end=" ")
         else:
             print("{:d}".format(s), end=" ")
-
