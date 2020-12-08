a59eb44be93cf81d6dec599b6a92ce49b47dfe91
JulianArbini97 <1890@holbertonschool.com>
2020-12-08 16:03:24 +0000

    python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for first in range(0, 9):
-       for second in range (second, 9):
+       for second in range (1, 10):
               if (second < first or second == first):
                      continue
                      if (first != 8):
