446887bf20fd8c2c9de331a85962bcf07ff43b82
JulianArbini97 <1890@holbertonschool.com>
2020-12-08 16:08:25 +0000

    python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for first in range(9):
-       for second in range(1, 10):
-              if (second < first or second == first):
-                     continue
-              if (first != 8):
-                     print("{:d}{:d}".format(first, second), end=', ')
-              else:
-                     print("{:d}{:d}".format(first, second))
+    for second in range(1, 10):
+        if (second < first or second == first):
+            continue
+        if (first != 8):
+            print("{:d}{:d}".format(first, second), end=', ')
+        else:
+            print("{:d}{:d}".format(first, second))
