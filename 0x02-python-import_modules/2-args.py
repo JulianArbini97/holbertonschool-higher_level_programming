0dae5885e9d3739b66ed990cc795af17c2a311f5
JulianArbini97 <1890@holbertonschool.com>
2020-12-10 16:21:52 +0000

    fix 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
-argc = len(sys.argv) - 1
-if argc == 1:
-    print('1 argument:')
-elif argc == 0:
-    print('0 arguments.')
-else:
-    print('{:d} arguments:'.format(argc))
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+    if argc == 1:
+        print('1 argument:')
+    elif argc == 0:
+        print('0 arguments.')
+    else:
+        print('{:d} arguments:'.format(argc))
 
-for arguments in range(1, argc + 1):
-    print('{:d}: {}'.format(arguments, sys.argv[arguments]))
+        for arguments in range(1, argc + 1):
+            print('{:d}: {}'.format(arguments, sys.argv[arguments]))
