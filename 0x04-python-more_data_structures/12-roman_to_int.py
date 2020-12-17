936d388ebe007b063b140896a16a010908c0fdb8
JulianArbini97 <1890@holbertonschool.com>
2020-12-17 20:34:34 +0000

    0x04

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    Dictionary_roman: {'I': 1, 'IV': 4, 'V': 5, 'IX': 9,
+    Dictionary_roman = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9,
                   'X': 10, 'XL': 40, 'L': 50, 'XC': 90,
                   'C': 100, 'CD': 400, 'D': 500, 'DM': 900,
                   'M': 1000}
-    res = 0;
-    a = 0;
+    res = 0
+    i = 0
 
-    if roman_string is not None and type(Dictionary_roman) is str:
-        while i < len(Dictionary_roman):
+    if roman_string is not None and type(roman_string) is str:
+        while i < len(roman_string):
             if Dictionary_roman.get(roman_string[i: i+2]) is not None:
-                res = res + Dictionary_roman.get(roman.string[i: i+2])
+                res = res + Dictionary_roman[roman_string[i: i+2]]
                 i = i + 2
             else:
-                res = res + Dictionary_roman.get(roman.string[i: i+2])
-                1 = i + 1
+                res = res + Dictionary_roman[roman_string[i]]
+                i = i + 1
     return res
