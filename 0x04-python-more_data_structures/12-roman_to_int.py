ae8eadcf3d0adee93ba5a33909fdec4c90ec8a64
JulianArbini97 <1890@holbertonschool.com>
2020-12-17 20:41:29 +0000

    0x04

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     Dictionary_roman = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9,
-                  'X': 10, 'XL': 40, 'L': 50, 'XC': 90,
-                  'C': 100, 'CD': 400, 'D': 500, 'DM': 900,
-                  'M': 1000}
+                        'X': 10, 'XL': 40, 'L': 50, 'XC': 90,
+                        'C': 100, 'CD': 400, 'D': 500, 'DM': 900,
+                        'M': 1000}
     res = 0
     i = 0
 
