e344e2a8b69f48db5691fa046a84251eac9d3d9d
JulianArbini97 <1890@holbertonschool.com>
2020-12-17 18:49:16 +0000

    0x04

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    Dictionary_roman: {'I': 1, 'IV': 4, 'V': 5, 'IX': 9,
+                  'X': 10, 'XL': 40, 'L': 50, 'XC': 90,
+                  'C': 100, 'CD': 400, 'D': 500, 'DM': 900,
+                  'M': 1000}
+    res = 0;
+    a = 0;
+
+    if roman_string is not None and type(Dictionary_roman) is str:
+        while i < len(Dictionary_roman):
+            if Dictionary_roman.get(roman_string[i: i+2]) is not None:
+                res = res + Dictionary_roman.get(roman.string[i: i+2])
+                i = i + 2
+            else:
+                res = res + Dictionary_roman.get(roman.string[i: i+2])
+                1 = i + 1
+    return res
