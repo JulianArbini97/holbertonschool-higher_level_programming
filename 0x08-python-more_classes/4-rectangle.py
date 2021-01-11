1d0a6f8a65e2bef1a421a4d7b80e623649af4a2f
JulianArbini97 <1890@holbertonschool.com>
2021-01-11 22:41:46 +0000

    fix 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -11,6 +11,15 @@ class Rectangle:
         self.width = width
         self.height = height
 
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+
     @property
     def width(self):
         """ Retrieve the width of rectangle """
