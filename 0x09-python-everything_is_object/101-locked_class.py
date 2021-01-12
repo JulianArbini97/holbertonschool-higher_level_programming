3d2588d5e27961e24cb91878a4fb19a053006cd2
JulianArbini97 <1890@holbertonschool.com>
2021-01-12 21:07:55 +0000

    advanced

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Comment
+"""
+
+class LockedClass:
+    """ comment """
+    __slots__ = ['first_name']
+
+    def __init__(self, first_name=""):
+        """ comment """
+        self.first_name = first_name
