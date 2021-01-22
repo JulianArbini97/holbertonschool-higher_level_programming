9ab5aaf6b151f110ac60934d800c78839923acd7
JulianArbini97 <1890@holbertonschool.com>
2021-01-22 18:54:54 +0000

    triangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Rectangle file """
+""" Rectangle file"""
 from . base import Base
 
 
@@ -84,8 +84,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """ values representation """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
         """Updates rectangle"""
