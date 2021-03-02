ed3a1fc03f36cf73eab9671e338da2706cfe55a4
JulianArbini97 <1890@holbertonschool.com>
2021-03-02 18:15:49 +0000

    11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
 -- script that lists all shows contained in the database hbtn_0d_tvshows
-SELECT serie.title, genero.genre_id FROM  tv_show_genres genero
-INNER JOIN tv_shows serie ON genero.show_id = serie.id
+SELECT serie.title, genero.genre_id FROM  tv_show_genres genero 
+RIGHT OUTER JOIN tv_shows serie ON genero.show_id = serie.id 
 ORDER BY serie.title, genero.genre_id ASC;
