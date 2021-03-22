cd174c65d034720a0636f52603523a221732c637
JulianArbini97 <1890@holbertonschool.com>
2021-03-22 17:12:11 +0000

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,12 +2,12 @@
 const arg = process.argv;
 
 function fact (x) {
-    if (isNaN(x) === true) {
-        return 1;
-    } else if (x > 0) {
-        return (x * fact(x - 1));
-    }
+  if (isNaN(x) === true) {
     return 1;
+  } else if (x > 0) {
+    return (x * fact(x - 1));
   }
+  return 1;
+}
 
 console.log(fact(arg[2]));
