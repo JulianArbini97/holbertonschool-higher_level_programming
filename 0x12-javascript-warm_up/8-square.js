a7b32bb25ffd7958a1f7f42125f6fdc8efa36d92
JulianArbini97 <1890@holbertonschool.com>
2021-03-22 15:29:16 +0000

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,8 +2,8 @@
 const arg = process.argv;
 let i;
 
-if (isNaN(arg[2])) {
-  console.log('Missing number of occurrences');
+if (isNaN(arg[2]) === true) {
+  console.log('Missing size');
 } else {
   for (i = 0; i < arg[2]; i++) {
     console.log('X'.repeat(arg[2]));
