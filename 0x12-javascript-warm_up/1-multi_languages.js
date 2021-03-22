0544a2e1bf0794278c2c6c641a8fe5193d0d54d7
JulianArbini97 <1890@holbertonschool.com>
2021-03-22 13:34:40 +0000

    1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+
+function logVar () {
+  console.log(firstLine);
+  console.log(secondLine);
+  console.log(thirdLine);
+}
+
+logVar();
