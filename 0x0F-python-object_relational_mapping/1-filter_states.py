cfa2f9c0dd706357786613803dcb81af9277d33f
JulianArbini97 <1890@holbertonschool.com>
2021-03-27 13:48:21 +0000

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""This module makes a MySQL query."""
-import MySQLdb
+""" Script that lists states with a name starting with N """
 from sys import argv
-
+import MySQLdb
 
 if __name__ == "__main__":
-    # Open connection to database.
-    db = MySQLdb.connect(host="localhost", user=argv[1],
-                         passwd=argv[2], db=argv[3], port=3306)
-    cursor = db.cursor()
-# MySQL Query.
-    sql = "SELECT * FROM states \
-           WHERE name LIKE 'N%' \
-           ORDER BY states.id ASC;"
-# Execute query.
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-# Close the connections.
-    cursor.close()
-    db.close()
+    My_host = "localhost"
+    My_user = argv[1]
+    My_pass = argv[2]
+    My_db = argv[3]
+    MyDataBase = MySQLdb.connect(host=My_host, user=My_user, passwd=My_pass,
+                                 db=My_db, port=3306, charset="utf8")
+    The_cursor = MyDataBase.cursor()
+    The_cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = The_cursor.fetchall()
+    for row in rows:
+        if row[1][0] == "N":
+            print(row)
+    The_cursor.close()
+    MyDataBase.close()
