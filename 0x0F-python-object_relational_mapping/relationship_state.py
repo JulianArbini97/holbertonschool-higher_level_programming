69fbc1bba4a0f3354a155ef33bf3849603cc4bf5
JulianArbini97 <1890@holbertonschool.com>
2021-03-29 02:11:16 +0000

    100

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -9,6 +9,6 @@ Base = declarative_base()
 class State(Base):
     """ Base class """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    cities = relationship("City", backref="state", cascade="delete")
