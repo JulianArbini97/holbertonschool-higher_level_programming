448687300bd51c75ad51d7e4cea59f082681ce2f
JulianArbini97 <1890@holbertonschool.com>
2021-04-08 18:44:26 +0000

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Test function find_peak """
 
+
 def find_peak(integer_list):
     """ Finding the peak of a list """
-
     """ Board cases """
     # middle_point = len(integer_list)/2
     # print (middle_point)
@@ -19,7 +19,7 @@ def find_peak(integer_list):
         middle_point = int(len(integer_list)/2)
     else:
         middle_point = int((len(integer_list) - 1)/2)
-    
+
     # print("PUNTO MEDIO: {}".format(middle_point))
     middle = integer_list[middle_point]
     next_int = integer_list[middle_point + 1]
@@ -27,7 +27,7 @@ def find_peak(integer_list):
     first_half = integer_list[:middle_point]
     # print("PRIMERA MITAD {}".format(first_half))
     second_half = integer_list[middle_point + 1:]
-    # print("SEGUNDA MITAD {}".format(second_half))
+    print("SEGUNDA MITAD {}".format(second_half))
 
     if middle > prev_int and middle > next_int:
         return middle
