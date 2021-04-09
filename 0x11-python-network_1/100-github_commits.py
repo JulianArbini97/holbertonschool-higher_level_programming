cc6ef833da1e72d7ebb69bb52da0e30811e39dbe
JulianArbini97 <1890@holbertonschool.com>
2021-04-09 18:28:11 +0000

    100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Script takes your GitHub and uses the GitHub API to display your Id """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    page = 'https://api.github.com/repos/' + argv[1] + '/' + argv[2] + '/commits'
+    r = requests.get(page)
+    id_result = r.json()
+    cont = 0
+    for i in id_result:
+        if cont < 10:
+            if type(i) is dict:
+                print("{}: {}".format(i['sha'], i['commit']['author']['name']))
+            cont += 1
+        else:
+            break
