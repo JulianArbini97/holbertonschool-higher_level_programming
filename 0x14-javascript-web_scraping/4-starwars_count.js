83f9ccdb791a8c215431541a753ac9d15141bf0c
JulianArbini97 <julianarbini97@gmail.com>
2021-05-04 17:08:19 +0000

    4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+let appearences = 0;
+
+request(url, function (err, data, body) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const iterable = JSON.parse(body).results;
+  const listlen = iterable.length;
+  let cont = 0;
+  let cont2 = 0;
+  for (cont = 0; cont < listlen; cont++) {
+    for (cont2 = 0; cont2 < iterable[cont].characters.length; cont2++) {
+      if (iterable[cont].characters[cont2].includes('/18/')) {
+        appearences += 1;
+      }
+    }
+  }
+  console.log(appearences);
+});
